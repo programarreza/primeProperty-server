2fa984faedf0a1d317f3efabfc64b937a3abe9d6
programarreza <programarreza@gmail.com>
2024-01-23 20:36:26 +0600

    added to login post api

--- app.js
+++ app.js
@@ -45,7 +45,50 @@ app.post("/api/register", async (req, res, next) => {
   }
 });
 
+app.post("/api/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.send(400).send("Please fill all required fields");
+    } else {
+      const user = await Users.findOne({ email });
+      if (!user) {
+        res.status(400).send("User email or password is incorrect");
+      } else {
+        const validateUser = await bcryptjs.compare(password, user.password);
+        if (!validateUser) {
+          res.status(400).send("User email or password is incorrect");
+        } else {
+          const payload = {
+            userId: user._id,
+            email: user.email,
+          };
+          const JWT_SECRET_KEY =
+            process.env.JWT_SECRET_KEY || "THIS_IS_A_JWT_SECRET_KEY";
+          jwt.sign(
+            payload,
+            JWT_SECRET_KEY,
+            { expiresIn: 84600 },
+            async (err, token) => {
+              await Users.updateOne(
+                { _id: user._id },
+                {
+                  $set: { token },
+                }
+              );
+              user.save();
+              next();
+            }
+          );
 
+          res.status(200).json({ user: {email:user.email, fullName: user.fullName, number: user.number}, token: user.token });
+        }
+      }
+    }
+  } catch (error) {
+    console.log("Error", error);
+  }
+});
 
 app.listen(port, () => {
   console.log(`prime property server port ${port}`);
